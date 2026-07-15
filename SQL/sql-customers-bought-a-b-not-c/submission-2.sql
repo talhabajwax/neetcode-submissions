a1bd0bb0fc2888a7a30429918793a5631a729874
Talha Ansar <93472275+talhabajwax@users.noreply.github.com>
2026-07-15 14:47:07 +0500

    Add: sql-customers-bought-a-b-not-c - submission-2

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-2.sql
@@ -0,0 +1,13 @@
+SELECT
+    c.customer_id,
+    c.customer_name
+FROM customers c
+JOIN orders o
+    ON c.customer_id = o.customer_id
+GROUP BY
+    c.customer_id,
+    c.customer_name
+HAVING COUNT(*) FILTER (WHERE o.product_name = 'A') > 0
+   AND COUNT(*) FILTER (WHERE o.product_name = 'B') > 0
+   AND COUNT(*) FILTER (WHERE o.product_name = 'C') = 0
+ORDER BY c.customer_name;
